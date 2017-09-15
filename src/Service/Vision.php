0cbd2094858c902578fa80914da86fa52c5300a9
dakku <dakku@97634.no-reply.drupal.org>
2017-09-15 11:37:16 +0100

    extending vision analyse

--- src/Service/Vision.php
+++ src/Service/Vision.php
@@ -17,12 +17,54 @@ class Vision {
   }
 
   // See https://westus.dev.cognitive.microsoft.com/docs/services/56f91f2d778daf23d8ec6739/operations/56f91f2e778daf14a499e1ff
-  public function analyze() {}
+  public function analyze($photoUrl,
+                          $visualFeatures = TRUE,
+                          $details = TRUE
+  ) {
+    $uri = self::API_URL . 'analyze';
+    $params = [];
+
+    if ($details) {
+      $params['details'] = implode(',', self::allowedDetails());
+    }
+    if ($visualFeatures) {
+      $params['visualFeatures'] = implode(',', self::allowedVisualFeatures());
+    }
+
+    if (count($params) > 0) {
+      $queryString = http_build_query($params);
+      $uri = urldecode($uri . '?' . $queryString);
+    }
+
+    $result = $this->client->doRequest($uri, 'POST', ['url' => $photoUrl]);
+
+    return $result;
+  }
+
   public function describe() {}
   public function generateThumbnail() {}
   public function ocr() {}
   public function models() {}
   public function recognizeText() {}
   public function tag() {}
+
+  private function allowedVisualFeatures() {
+    return [
+      'Categories',
+      'Tags',
+      'Description',
+      'Faces',
+      'ImageType',
+      'Color',
+      'Adult'
+    ];
+  }
+
+  private function allowedDetails() {
+    return [
+      'Celebrities',
+      'Landmarks'
+    ];
+  }
 
 }
