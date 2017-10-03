e79626e081b0de4fb58f212111563bdb205ed8a6
dakku <dakku@97634.no-reply.drupal.org>
2017-10-03 21:51:05 +0100

    Added additional methods.

--- src/Service/Vision.php
+++ src/Service/Vision.php
@@ -61,40 +61,97 @@ class Vision {
       $uri = urldecode($uri . '?' . $queryString);
     }
 
-    $body = ['json' => ['url' => $photoUrl]];
-    $result = $this->azureClient->doRequest('vision', $uri, 'POST', $body);
-
-    return $result;
+    return self::doRequest($uri, $photoUrl);
   }
 
   /**
+   * See https://westus.dev.cognitive.microsoft.com/docs/services/56f91f2d778daf23d8ec6739/operations/56f91f2e778daf14a499e1fe.
    *
+   * @param $photoUrl
+   *
+   * @return bool|mixed
    */
-  public function describe() {}
+  public function describe($photoUrl) {
+    $uri = $this->config->get('api_url') . 'describe';
+    return self::doRequest($uri, $photoUrl);
+  }
 
   /**
+   * See https://westus.dev.cognitive.microsoft.com/docs/services/56f91f2d778daf23d8ec6739/operations/56f91f2e778daf14a499e1fb.
    *
+   * @param $photoUrl
+   * @param int $width
+   * @param int $height
+   * @param bool $smartCropping
+   *
+   * @return bool|mixed
    */
-  public function generateThumbnail() {}
+  public function generateThumbnail($photoUrl,
+                                       $width = 100,
+                                       $height = 100,
+                                       $smartCropping = TRUE
+  ) {
+    $params = [];
+    $params['width'] = $width;
+    $params['height'] = $height;
+    $params['smartCropping'] = $smartCropping;
+    $uri = $this->config->get('api_url') . 'generateThumbnail';
+
+    if (count($params) > 0) {
+      $queryString = http_build_query($params);
+      $uri = urldecode($uri . '?' . $queryString);
+    }
+    // TODO Check the return value and handle binary data
+    return self::doRequest($uri, $photoUrl);
+  }
 
   /**
+   * See https://westus.dev.cognitive.microsoft.com/docs/services/56f91f2d778daf23d8ec6739/operations/56f91f2e778daf14a499e1fc.
    *
+   * @param $photoUrl
+   *
+   * @return bool|mixed
    */
-  public function ocr() {}
+  public function ocr($photoUrl) {
+    $uri = $this->config->get('api_url') . 'ocr';
+    return self::doRequest($uri, $photoUrl);
+  }
 
   /**
+   * See https://westus.dev.cognitive.microsoft.com/docs/services/56f91f2d778daf23d8ec6739/operations/587f2c6a154055056008f200.
    *
+   * @param $photoUrl
+   *
+   * @return bool|mixed
    */
-  public function models() {}
+  public function recognizeText($photoUrl) {
+    $uri = $this->config->get('api_url') . 'recognizeText';
+    // TODO Check the response value and return the ID
+    return self::doRequest($uri, $photoUrl);
+  }
 
   /**
+   * See https://westus.dev.cognitive.microsoft.com/docs/services/56f91f2d778daf23d8ec6739/operations/56f91f2e778daf14a499e1ff.
    *
+   * @param $photoUrl
+   *
+   * @return bool|mixed
    */
-  public function recognizeText() {}
+  public function tag($photoUrl) {
+    $uri = $this->config->get('api_url') . 'tag';
+    return self::doRequest($uri, $photoUrl);
+  }
 
   /**
+   * @param $uri
+   * @param $photoUrl
    *
+   * @return bool|mixed
    */
-  public function tag() {}
+  private function doRequest($uri, $photoUrl) {
+    $body = ['json' => ['url' => $photoUrl]];
+    $result = $this->azureClient->doRequest('vision', $uri, 'POST', $body);
+    return $result;
+  }
 
 }
